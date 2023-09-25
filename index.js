402c870acf598ce05ac3bdd61943da73d437bb37
ckorir <chrisruto.k@gmail.com>
2023-09-25 21:39:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record){
+    let gamesWon = record.find(game => game.result === "W");
+
+    if (gamesWon){
+        return gamesWon.year;
+    }else{
+        return undefined;
+    }
+}
